7d47e3afd009766e373ce112b469b2ef60533973
Vitaly Kovalev <kovalevvitalii@mail.ru>
2023-02-12 14:01:30 +0300

    Initial commit

---
+++ Person.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Events
+{
+    public class Person
+    {
+        public event Action GoToSleep;
+        public event EventHandler DoWork;
+        public string Name { get; set; }
+
+        public void TakeTime(DateTime now)
+        {
+            if (now.Hour <= 8)
+            {
+                GoToSleep?.Invoke();
+            }
+            else
+            {
+                var args = new EventArgs();
+                DoWork?.Invoke(this, null);
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Events
+{
+    internal class Program
+    {
+        public delegate void MyDelegate(); 
+        public event MyDelegate Event;
+        public event Action EventAction;
+        public static void Main(string[] args)
+        {
+            Person person = new Person();                             //создали персону
+            person.Name = "Вася";
+            person.GoToSleep += Person_GoToSleep;                     //подписали на событие 
+            person.DoWork += Person_DoWork;
+            person.TakeTime(DateTime.Parse("12.02.2023 13:41"));  //вызываем метод два раза
+            person.TakeTime(DateTime.Parse("12.02.2023 5:41"));
+        }
+
+        private static void Person_DoWork(object sender, EventArgs e)
+        {
+            if (sender is Person)
+            {
+                Console.WriteLine($"{((Person)sender).Name} работает работу");
+            }
+        }
+
+        private static void Person_GoToSleep()
+        {
+            Console.WriteLine("Человек идет спать");
+            Console.ReadLine();
+        }
+    }
+}
